9e6ff865e86b15c70255e9cbb578c9be84105024
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2024-08-03 17:07:36 -0700

    e

--- firebase.ts
+++ firebase.ts
@@ -6,7 +6,8 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  projectId: "firebasics-22cc2",
+  // projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
